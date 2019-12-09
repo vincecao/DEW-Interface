769baa16a2fbb648fe0273b7fc52c5f2a0e6bb39
Lineng Cao <linengca@usc.edu>
2019-12-09 14:04:18 -0800

    Update: Clear inside Behavior Card

--- src/Components/hlbComponent/HlbDiv.js
+++ src/Components/hlbComponent/HlbDiv.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import ResultCard from '../parts/ResultCard'
 import { Button, InputGroup, FormControl, Form } from 'react-bootstrap'
 import { Position, Tooltip, Intent } from "@blueprintjs/core";
-import { faTimes, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { faTimes, faTimesCircle, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default class HlbDiv extends Component {
@@ -11,7 +11,7 @@ export default class HlbDiv extends Component {
     super(props)
     this.state = {
       behaviorCount: 1,
-      behaviorInputs: [['', this.behaviorInputGenerator(1)]],
+      behaviorInputs: ['Default Line1', 'Default Line2'],
       currentActive: -1,
       currentCard: 'Actors',
       behaviorSuggestionLst: ['wait t', 'emit'],
@@ -19,74 +19,72 @@ export default class HlbDiv extends Component {
     }
   }
 
-
-
-  behaviorInputGenerator = (index) => {
-    return ['', <div style={{ display: 'flex', flexDirection: 'row' }}><InputGroup className="mb-3" key={'inputGroup-' + index}>
-      <InputGroup.Prepend>
-        <Tooltip content="Press return to create a new behavior" position={Position.RIGHT}>
-          <InputGroup.Text className='btn-light border-0'>{index}</InputGroup.Text>
-        </Tooltip>
-      </InputGroup.Prepend>
-      <FormControl aria-label="Amount (to the nearest dollar)" onClick={() => this.handleOnClickWithSuggestionChange('Behavior')} text={(this.state === undefined || this.state.behaviorInputs[index - 1] === undefined) ? '' : this.state.behaviorInputs[index - 1][0]} onKeyPress={target => this.handleKeyPress(target)} onChange={(e) => { this.handleBehaviorValueOnChange(e, index) }} />
-      <InputGroup.Append>
-        <Button variant="outline-danger" className='btn-light border-0' onClick={() => this.handleOnInputClear(index)}><FontAwesomeIcon icon={faTimesCircle} /></Button>
-      </InputGroup.Append>
-    </InputGroup><Button variant="outline-danger" className='btn-light border-0 ml-3' style={{ height: 38 }} onClick={() => this.handleOutInputClear(index)}><FontAwesomeIcon icon={faTimes} /></Button></div>]
-  }
-
   handleOnInputClear = (index) => {
-    console.log('clear in input ' + (index - 1))
+    console.log('clear in input ' + index)
     let temp = this.state.behaviorInputs
-    temp[index - 1][0] = ''
+    temp[index] = ''
     this.setState({
       ...this.states,
       behaviorInputs: temp
     })
     this.props.addToast(Intent.WARNING, "Clear one line")
-    //ToDo: Text of input hard coded during first Generator
   }
 
   handleOutInputClear = (index) => {
-    console.log('clear out input ' + (index - 1))
+    console.log('clear out input ' + index)
+
+    let temp = this.state.behaviorInputs
+    temp.splice(index, 1)
+    this.setState({
+      ...this.states,
+      behaviorInputs: temp
+    })
+
     this.props.addToast(Intent.DANGER, "Remove one line")
+
   }
 
   handleBehaviorValueOnChange = (e, index) => {
     let temp = this.state.behaviorInputs
-    temp[index - 1][0] = e.target.value
+    temp[index] = e.target.value
     this.setState({
       ...this.state,
       behaviorInputs: temp
     })
   }
 
-  handleKeyPress = (target) => {
+  handleKeyPress = (target, index) => {
     if (target.charCode === 13) {
-      if (this.state.behaviorInputs[this.state.behaviorInputs.length - 1][0] !== '') {
+      if (this.state.behaviorInputs[index] === '' || (this.state.behaviorInputs && this.state.behaviorInputs[index + 1] === '')) {
+        this.props.addToast(Intent.DANGER, "Please fill line first")
+      } else {
+        let temp = this.state.behaviorInputs
+        temp.splice(index + 1, 0, '')
         this.setState({
           ...this.state,
           behaviorCount: this.state.behaviorCount + 1,
-          behaviorInputs: [...this.state.behaviorInputs, ['', this.behaviorInputGenerator(this.state.behaviorCount + 1)]]
+          behaviorInputs: temp
         })
-      } else {
-        this.props.addToast(Intent.DANGER, "Current line is empty")
       }
     }
-
-    // if (target.charCode == 13) {
-    //   let temp = this.state.behaviorInputs
-    //   temp.splice(1, 0, ['',this.behaviorInputGenerator(this.state.behaviorCount + 1)])
-    //   this.setState({
-    //     ...this.state,
-    //     behaviorCount: this.state.behaviorCount + 1,
-    //     behaviorInputs: temp
-    //   })
-    // }
   }
 
   renderbehaviorInputs = () => {
-    return this.state.behaviorInputs.map((input) => input[1])
+    if (this.state.behaviorInputs.length === 0) return <Button className='btn-light' onClick={() => this.setState({ ...this.state, behaviorInputs: [''] })}><FontAwesomeIcon icon={faPlus} /></Button>
+    return this.state.behaviorInputs.map((input, index) => <div style={{ display: 'flex', flexDirection: 'row' }} key={'div-inputGroup-' + index}>
+      <InputGroup className="mb-3" >
+        <InputGroup.Prepend>
+          <Tooltip content="Press return to create a new behavior" position={Position.RIGHT}>
+            <InputGroup.Text className='btn-light border-0'>{index + 1}</InputGroup.Text>
+          </Tooltip>
+        </InputGroup.Prepend>
+        <FormControl aria-label="Amount (to the nearest dollar)" onClick={() => this.handleOnClickWithSuggestionChange('Behavior')} value={this.state.behaviorInputs[index]} onKeyPress={target => this.handleKeyPress(target, index)} onChange={(e) => { this.handleBehaviorValueOnChange(e, index) }} />
+        <InputGroup.Append>
+          <Button variant="outline-danger" className='btn-light border-0' onClick={() => this.handleOnInputClear(index)}><FontAwesomeIcon icon={faTimesCircle} /></Button>
+        </InputGroup.Append>
+      </InputGroup><Button variant="outline-danger" className='btn-light border-0 ml-3' style={{ height: 38 }} onClick={() => this.handleOutInputClear(index)}><FontAwesomeIcon icon={faTimes} />
+      </Button>
+    </div>)
   }
 
 
@@ -127,9 +125,9 @@ export default class HlbDiv extends Component {
       <Button className='btn-primary' style={{ margin: 5 }}>Suggestion</Button>
     </>
 
-    if (this.state.currentCard === 'Behavior') return this.state.behaviorSuggestionLst.map(item => <Button className='btn-secondary' style={{ margin: 5 }}>{item}</Button>)
+    if (this.state.currentCard === 'Behavior') return this.state.behaviorSuggestionLst.map((item, index) => <Button key={'behavior-suggestion-' + index} className='btn-secondary' style={{ margin: 5 }}>{item}</Button>)
 
-    if (this.state.currentCard === 'Constraints') return this.state.constraintsSuggestionLst.map(item => <Button className='btn-warning' style={{ margin: 5 }}>{item}</Button>)
+    if (this.state.currentCard === 'Constraints') return this.state.constraintsSuggestionLst.map((item, index) => <Button key={'constraints-suggestion-' + index} className='btn-warning' style={{ margin: 5 }}>{item}</Button>)
 
   }
 
@@ -138,7 +136,7 @@ export default class HlbDiv extends Component {
     return (
       <>
         <div style={{ flex: 1, display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-          <div style={{ flex: 1 }}>
+          <div style={{ flex: 2 }}>
             <ResultCard bg='info' header='Actors' render={this.renderActorCard()} />
             <ResultCard bg='secondary' header='Behavior' render={this.renderbehaviorInputs()} />
             <ResultCard bg='warning' header='Constraints' render={this.renderConstraintCard()} />
